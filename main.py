9d127a86ac80274fe8f4d4e7e4abb4a1e0cf53a8
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-11-08 12:18:59 -0300

    add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from orchestrator.orchestrator import Orchestrator
-from teams.team import Team
+from teams.teams import Team
 from agents.agent import Agent
 import asyncio
 
